f6f45a031aa1176948935985666e73b43577467b
bilal-dev2006 <elemranibilal2006@gmail.com>
2023-07-31 19:34:21 +0100

    new version of code

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 // Project : Digital Clock
-const [hours, minutes, seconds, mode] = document.querySelectorAll("span");
+const time = document.querySelector(".dispaly");
+const btn = document.querySelector("p");
+const options = document.querySelectorAll('input[type="radio"]');
 
+btn.addEventListener("click", function () {
+  btn.classList.toggle("open");
+});
 // Function to update the time
 function updateTime() {
-  let date = new Date();
-  hours.textContent = date.getHours();
-  minutes.textContent = date.getMinutes();
-  seconds.textContent = date.getSeconds();
-  mode.textContent = +hours.textContent > 12 ? "PM" : "AM"; // ckeck AM or PM
-  // Checking if any of the time elements are single digit and adding a leading zero if so
-  [hours, minutes, seconds].forEach((element) => {
-    if (element.textContent.length === 1) {
-      element.textContent = "0" + element.textContent;
+  for (option of options) {
+    if (option.checked) {
+      let myDate = new Date().toLocaleString("en-Us", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: JSON.parse(option.getAttribute("h")),
+      });
+      time.textContent = myDate;
     }
-  });
+  }
 }
 // Updating the time every second
 setInterval(updateTime, 1000);
